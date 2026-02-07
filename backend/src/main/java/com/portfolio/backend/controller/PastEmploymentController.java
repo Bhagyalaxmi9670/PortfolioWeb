92816150b6dfe8754bf4eb8923b52c32b4b78722
Sonu9670 <bhagyalaxmirout530@gmail.com>
2026-02-07 20:47:50 +0530

    change

--- backend/src/main/java/com/portfolio/backend/controller/PastEmploymentController.java
+++ backend/src/main/java/com/portfolio/backend/controller/PastEmploymentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/employment")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PastEmploymentController {
 
     private final PastEmploymentService service;
